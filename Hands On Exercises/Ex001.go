dd57244d42e12febb64cdf74ed4c5cdffee6eda5
Matthew Yeager <mpyeager@gmail.com>
2022-05-09 12:56:51 +0100

    Initial commit

---
+++ Hands On Exercises/Ex001.go
@@ -0,0 +1,25 @@
+// Exercise 1
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// Using the short declaration operator, ASSIGN VALUES
+	// to VARIABLES with the identifiers "x", "y", and "z"
+	x := 42
+	y := "Bond. James Bond."
+	z := true
+
+	// Now print the values stored in those variables using a
+	// single statement
+	fmt.Println(x, y, z)
+
+	// AND multiple print statements
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+
+}
