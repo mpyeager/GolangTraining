0c17e753db134d6d383cf5e589b0591d45a3f703
Matthew Yeager <mpyeager@gmail.com>
2022-05-10 10:39:16 +0100

    initial commit

---
+++ Hands On Exercises/Level 2/Ex001.go
@@ -0,0 +1,14 @@
+// Level 2: Exercise 1
+// Write a program that prints a number in decimal, binary, and hex
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := 24
+	fmt.Printf("%d\t%b\t%#x\t", a, a, a)
+
+}
