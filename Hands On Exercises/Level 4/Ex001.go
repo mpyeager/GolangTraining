17b2e1d73c2e289ef2269f929ad443fcdcb0fd40
Matthew Yeager <mpyeager@gmail.com>
2022-05-13 19:03:44 +0100

    initial commit

---
+++ Hands On Exercises/Level 4/Ex001.go
@@ -0,0 +1,16 @@
+// Level 4: Exercise 1
+// Using a composite literal; create and array which holds 5 valuse of type int, assigne values to each index position. Range over the array and print the values out. Using format printing, print out the type of the array.
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := [5]int{27, 28, 29, 30, 31}
+	for i, v := range x {
+		fmt.Println(i, v)
+	}
+	fmt.Printf("%T\n", x)
+}
---
+++ Hands On Exercises/Level 4/README.md
@@ -0,0 +1,3 @@
+## Exercises in this folder
+1. [Ex001](Ex001.go)
+> Using a `composite literal`; create an `array` which holds 5 values of type `int`, assign values to each `index position`. `Range` over the array and print the values out. Using `format printing`, print out the `type` of the `array`.
