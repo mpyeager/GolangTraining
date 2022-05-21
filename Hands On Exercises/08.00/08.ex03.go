0dfacef728fc0cdc1f512c2707d6e2b9e4f645dd
Matthew Yeager <mpyeager@gmail.com>
2022-05-21 17:20:40 +0100

    initial commit

--- Hands On Exercises/08.00/08.ex03.go
+++ Hands On Exercises/08.00/08.ex03.go
@@ -1,10 +1,107 @@
-// Level x: Exercise x
-// Instruction set
+// Level 8: Exercise 2
+// Unmarshal JSON into a Go data structure.
 
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
 
-func main()  {
+type dooooooonut []struct {
+	ID      string  `json:"id"`
+	Type    string  `json:"type"`
+	Name    string  `json:"name"`
+	Ppu     float64 `json:"ppu"`
+	Batters struct {
+		Batter []struct {
+			ID   string `json:"id"`
+			Type string `json:"type"`
+		} `json:"batter"`
+	} `json:"batters"`
+	Topping []struct {
+		ID   string `json:"id"`
+		Type string `json:"type"`
+	} `json:"topping"`
+}
 
+func main() {
+	data := `[[
+		{
+			"id": "0001",
+			"type": "donut",
+			"name": "Cake",
+			"ppu": 0.55,
+			"batters":
+				{
+					"batter":
+						[
+							{ "id": "1001", "type": "Regular" },
+							{ "id": "1002", "type": "Chocolate" },
+							{ "id": "1003", "type": "Blueberry" },
+							{ "id": "1004", "type": "Devil's Food" }
+						]
+				},
+			"topping":
+				[
+					{ "id": "5001", "type": "None" },
+					{ "id": "5002", "type": "Glazed" },
+					{ "id": "5005", "type": "Sugar" },
+					{ "id": "5007", "type": "Powdered Sugar" },
+					{ "id": "5006", "type": "Chocolate with Sprinkles" },
+					{ "id": "5003", "type": "Chocolate" },
+					{ "id": "5004", "type": "Maple" }
+				]
+		},
+		{
+			"id": "0002",
+			"type": "donut",
+			"name": "Raised",
+			"ppu": 0.55,
+			"batters":
+				{
+					"batter":
+						[
+							{ "id": "1001", "type": "Regular" }
+						]
+				},
+			"topping":
+				[
+					{ "id": "5001", "type": "None" },
+					{ "id": "5002", "type": "Glazed" },
+					{ "id": "5005", "type": "Sugar" },
+					{ "id": "5003", "type": "Chocolate" },
+					{ "id": "5004", "type": "Maple" }
+				]
+		},
+		{
+			"id": "0003",
+			"type": "donut",
+			"name": "Old Fashioned",
+			"ppu": 0.55,
+			"batters":
+				{
+					"batter":
+						[
+							{ "id": "1001", "type": "Regular" },
+							{ "id": "1002", "type": "Chocolate" }
+						]
+				},
+			"topping":
+				[
+					{ "id": "5001", "type": "None" },
+					{ "id": "5002", "type": "Glazed" },
+					{ "id": "5003", "type": "Chocolate" },
+					{ "id": "5004", "type": "Maple" }
+				]
+		}
+	]]`
+
+	fmt.Println(data)
+
+	err := json.NewEncoder(os.Stdout).Encode(data)
+	if err != nil {
+		fmt.Println("Oops!", err)
+	}
 }
--- Hands On Exercises/08.00/08.ex04.go
+++ Hands On Exercises/08.00/08.ex04.go
@@ -1,10 +1,23 @@
-// Level x: Exercise x
-// Instruction set
+// Level 8: Exercise 4
+// Sort the int[] and string[].
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
-func main()  {
+func main() {
+	xi := []int{5, 8, 2, 43, 17, 987, 14, 12, 21, 1, 4, 2, 3, 93, 13}
+	xs := []string{`we`, `don't`, `make`, `mistakes`, `just`, `happy`, `little`, `accidents`, `there's`, `nothing`, `wrong`, `with`, `having`, `a`, `tree`, `as`, `a`, `friend`}
 
+	fmt.Println(xi)
+	sort.Ints(xi)
+	fmt.Println(xi)
+
+	fmt.Println(xs)
+	sort.Strings(xs)
+	fmt.Println(xs)
 }
+
