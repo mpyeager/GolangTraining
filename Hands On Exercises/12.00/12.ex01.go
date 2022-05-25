2002832dc4b56dacb01b0c3695e1e285ff8f5938
Matthew Yeager <mpyeager@gmail.com>
2022-05-25 13:18:54 +0100

    initial commit

---
+++ Hands On Exercises/12.00/12.ex01.go
@@ -0,0 +1,17 @@
+// Level 12: Exercise 1
+// Create a package dog where the package should have an expected func years whihc converts human years to dog years. Document your code with comments, then run program to ensure it is functional and then run local server with godoc to view documentation.
+
+package main
+
+type hund struct {
+	name 	string
+	age 	int
+}
+
+func main() {
+	dog := hund{
+		name: "Rafi",
+		age: dog.Years(10),
+	}
+	fmt.Println(Rafi)
+}
