8cc2b12e77378a57cb9a7d5c926f12f20beb9836
Matthew Yeager <mpyeager@gmail.com>
2022-05-25 15:43:01 +0100

    updated package imports

--- Hands On Exercises/12.00/kelev/main.go
+++
@@ -1,7 +0,0 @@
-// Package kelev provides ageing conversion for between dogs and humans.
-package kelev
-
-// Years converts human years to dog years.
-func Years(human int) int {
-	return human * 7
-}
